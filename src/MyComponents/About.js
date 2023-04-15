c7b527063c046e695f0c0f32d5da99ac043c5825
Nishant Tiwari <t-ntiwari@microsoft.com>
2023-04-16 01:58:38 +0530

    second commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div>
+        This is about Component
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda at harum aperiam numquam animi id reiciendis. Sunt, animi repudiandae? Aliquam, alias eum? Asperiores maiores dolorum quaerat dolores consequatur odit voluptatem optio accusamus debitis vel!
+        </p>
+    </div>
+  )
+}
